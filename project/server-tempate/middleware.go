19dbb7d476b9eb68f63f074071c66635e3484a43
SeanThakur <vikash_thakur@amperehourenergy.com>
2024-12-14 14:43:28 +0530

    added middleware function

---
+++ project/server-tempate/middleware.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func WriteConsole(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("request url", r.URL)
+		fmt.Println("request host", r.Host)
+		next.ServeHTTP(w, r)
+	})
+}
--- project/server-tempate/routes.go
+++ project/server-tempate/routes.go
@@ -5,14 +5,19 @@ import (
 
 	"github.com/SeanThakur/gopl/project/server-tempate/config"
 	"github.com/SeanThakur/gopl/project/server-tempate/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.APP_CONFIG) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.HomeHandler))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.AboutHandler))
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
+	mux.Use(WriteConsole)
+
+	mux.Get("/", handlers.Repo.HomeHandler)
+	mux.Get("/about", handlers.Repo.AboutHandler)
 
 	return mux
 }
